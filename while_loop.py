c15af160e8ff9ab1cc0f67bc4bbd29c228ccb1d7
Julian Bar <JBar@spartaglobal.com>
2019-10-30 11:56:50 +0000

    Final Final comment

--- while_loop.py
+++ while_loop.py
@@ -51,12 +51,12 @@
 #         print(fizzbuzz)
 
 
-for fizzbuzz in range(1, 51):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-    else:
-        print(fizzbuzz)
+# for fizzbuzz in range(1, 51):
+#     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+#         print("fizzbuzz")
+#     elif fizzbuzz % 3 == 0:
+#         print("fizz")
+#     elif fizzbuzz % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(fizzbuzz)
